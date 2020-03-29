a38adc794d6e2b06755589a4d6f93cc2e8b46aa4
Sombra <sombra-klg@yandex.ru>
2020-03-29 20:06:20 +0300

    созданы файлы для тестов

---
+++ test.js
@@ -0,0 +1,11 @@
+describe("pow", function() {
+
+  it("2 в 3 степени будет", function() {
+    assert.equal(pow(2, 3), 8);
+  });
+
+  it("3 в 4 степени будет", function() {
+    assert.equal(pow(3, 4), 81);
+  });
+
+});
